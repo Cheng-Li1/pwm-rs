3ae30d8b1cddc0fab153ee7fc32a53d8f8548481
Cheng Li <lichengchaoreng@gmail.com>
2025-10-21 17:33:38 +1100

    Add the implementation of sending pwm request.
    Signed-off-by: Cheng Li <lichengchaoreng@gmail.com>

--- pwm_protocol/pwm_trait.rs
+++ pwm_protocol/pwm_trait.rs
@@ -36,18 +36,30 @@ pub struct PwmRequest {
     pub polarity: PwmPolarity,
 }
 
+const NS_IN_S: u32 = 1000_000_000;
+
 pub trait PwmHal {
     /// Return the state of Pwm and the frequency of the clock applied to the pwm
     fn pwm_get_state(&mut self) -> Result<(PwmState, ClockFrequency), PwmError> {
         Err(PwmError::ENOTIMPLEMENTED)
     }
 
-    // Send request will enable the disabled pwm automatically
-    fn pwm_send_request(&mut self, request: PwmRequest) -> Result<(), PwmError> {
+    fn pwm_apply_state(&mut self, state: PwmState) -> Result<(), PwmError> {
         Err(PwmError::ENOTIMPLEMENTED)
     }
 
-    fn pwm_apply_state(&mut self, state: PwmState) -> Result<(), PwmError> {
-        Err(PwmError::ENOTIMPLEMENTED)
+    // Send request will enable the disabled pwm automatically
+    fn pwm_send_request(&mut self, request: PwmRequest) -> Result<(), PwmError> {
+        let (_, clock) = self.pwm_get_state().unwrap();
+
+        let duty_cycle = (request.duty_ns * clock) / NS_IN_S;
+
+        let period_cycle = (request.period_ns * clock) / NS_IN_S;
+
+        self.pwm_apply_state(PwmState::Enable(PwmRawState {
+            polarity: request.polarity,
+            period_cycle,
+            duty_cycle,
+        }))
     }
 }
